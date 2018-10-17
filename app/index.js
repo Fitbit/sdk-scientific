9bd82d9060f4301bdf22e07d10c5e8f7102e21f1
Jon Barlow <jon.barlow@fitbit.com>
2018-10-17 16:54:42 +0100

    Tidy code formatting

--- app/index.js
+++ app/index.js
@@ -10,18 +10,26 @@ import Disc from "./disc";
 display.autoOff = false;
 display.on = true;
 
+// 3 circles
 const disc1 = new Disc(document.getElementById("disc1"), 30, "fb-red");
 const disc2 = new Disc(document.getElementById("disc2"), 26, "fb-yellow");
 const disc3 = new Disc(document.getElementById("disc3"), 22, "fb-green");
 
 // 5hz lowpass butterworth filter
-const coeffA = new Float32Array([1, -2.37409474, 1.92935567, -0.53207537]);
+const coeffA = new Float32Array([
+  1,
+  -2.37409474,
+  1.92935567,
+  -0.53207537
+]);
 const coeffB = new Float32Array([
   0.00289819,
   0.00869458,
   0.00869458,
   0.00289819
 ]);
+
+// An instance of the filter for each axis
 const filter5hzX = new LinearFilter(coeffB, coeffA);
 const filter5hzY = new LinearFilter(coeffB, coeffA);
 
